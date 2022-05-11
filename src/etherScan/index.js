4e1b7f451ed941c08eec238ae26d079c8eb97258
Stanley Yuen <stanley.yuen@consensys.net>
2022-05-11 15:38:42 +0800

    feat: find deployer from etherscan
     - add find deployer from etherscan

---
+++ src/etherScan/index.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import config from '../config/etherscan'
+let axiosInstance = null
+const getInstance = () => {
+    if(axiosInstance === null)
+    {
+        axiosInstance = axios.create({
+            baseURL: config.uri,
+            timeout: 3000
+        });
+    }
+    return axiosInstance;
+}
+
+export const getDeployer = async(address) => {
+    const i = getInstance();
+    const params = {
+        module: 'account',
+        action: 'txlist',
+        address,
+        page: 1,
+        offset:1,
+        sort:'asc',
+        apikey:config.apiKey
+    };
+  
+   const r =  await i.get('', {params});
+   return r.data ? r.data.result[0] : null
+}
+
