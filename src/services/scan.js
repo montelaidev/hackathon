4845108b1faffae79af8c509a6bc883dfa5c3255
Stanley Yuen <stanley.yuen@consensys.net>
2022-05-12 16:42:17 +0800

    Update scan.js

--- src/services/scan.js
+++ src/services/scan.js
@@ -22,7 +22,7 @@ export const scan = async(contractAddress) => {
         await write(list, s3Config.key)
         return [from, list[from]]
     }
-    return null
+    return [null, null]
 }
 
 export const computeRiskLevel = (data) =>{
