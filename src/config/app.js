cf439833e5b2e79b87cb1800752e31952838a02f
Stanley Yuen <stanley.yuen@consensys.net>
2022-05-12 16:38:37 +0800

    chore: add risk level calculation
    add risk level calculation

--- src/config/app.js
+++ src/config/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 export default {
   port: process.env.PORT || 9000,
   initScan: process.env.INIT_DATA || '0',
-  provider: process.env.PROVIDER || "ws://localhost:8546"
+  provider: process.env.PROVIDER || "ws://localhost:8546",
+  nonceLevel: process.env.NONCE_LEVEL || 20
 }
   
