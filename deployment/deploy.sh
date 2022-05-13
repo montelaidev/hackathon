297fb873378c6730b701dafbedaccacf9f465c61
Stanley Yuen <stanley.yuen@consensys.net>
2022-05-13 16:27:17 +0800

    chore: add deployment script
    chore: add deployment script

---
+++ deployment/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Enter Version: "
+read VERSION
+
+echo "Enter ENV File Path: "
+read ENV
+
+sudo docker kill $(docker ps -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker rmi $(docker images -q)
+sudo docker ps -al
+
+
+echo "run sudo docker build . -t hackathon-ws:$VERSION -f Dockerfile.Subscription"
+sudo docker build . -t hackathon-ws:$VERSION -f Dockerfile.Subscription
+sudo docker run -itd --rm --env-file $ENV hackathon-ws:$VERSION
+
+echo "run sudo docker build . -t hackathon-ws:$VERSION -f Dockerfile.Subscription"
+sudo docker build . -t hackathon:$VERSION -f Dockerfile
+sudo docker run -itd --rm --env-file $ENV -p 80:9000 hackathon:$VERSION
